10cefd33f647413e34ed72f6ac911df2bc8bc20b
Zahrya Rozi <zahryarozi@gmail.com>
2026-04-03 22:34:01 -0500

    Added Vercel Analytics out of curiosity

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Plus_Jakarta_Sans, Be_Vietnam_Pro } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
